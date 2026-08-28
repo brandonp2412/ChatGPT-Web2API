397d894b4ed392bda0b5e719beacbd5f060b4d55
Brandon Dick <brandon@presley.nz>
2026-08-28 13:38:36 +1200

    test: rename app package to Sloppa

--- client/test/app_smoke_test.dart
+++ client/test/app_smoke_test.dart
@@ -1,15 +1,15 @@
-import 'package:chatgpt_bridge_client/main.dart';
-import 'package:chatgpt_bridge_client/src/state/chat_controller.dart';
-import 'package:chatgpt_bridge_client/src/storage/secure_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sloppa/main.dart';
+import 'package:sloppa/src/state/chat_controller.dart';
+import 'package:sloppa/src/storage/secure_store.dart';
 
 void main() {
   testWidgets(
     'full app shell renders before initialization',
     (WidgetTester tester) async {
       final controller = ChatController(store: SecureStore());
-      await tester.pumpWidget(ChatBridgeApp(controller: controller));
+      await tester.pumpWidget(SloppaApp(controller: controller));
 
       expect(find.byType(MaterialApp), findsOneWidget);
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
